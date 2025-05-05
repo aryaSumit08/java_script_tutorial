d087e584b2faf9670e33346fb11cdcbc073d373a
aryaSumit08 <59229043+aryaSumit08@users.noreply.github.com>
2025-05-05 21:14:41 +0000

    variable 1 done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = "12345";
+let email = "sumit@gmail.com";
+var city = "Delhi";
+password ="6789";
+let x;
+console.table([accountId ,email ,city,x]);
+/*
+
+Prefer not to use var because of issue in scope
+
+*/
+//accountId = 2; // cannot be chaged as it is declared const
